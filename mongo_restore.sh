7d5f2382a40d696a7ed2db4944ead389e3868375
Sagar <sagar.devices@gmail.com>
2024-08-05 11:49:59 +0530

    updated restore script

--- mongo_restore.sh
+++ mongo_restore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# mongo_backup.sh
+# mongo_restore.sh
 
 # Install Environment file
 source .env 
 
-# Backup MongoDB
+# Restore MongoDB
 /usr/bin/docker exec mongodb_acc mongorestore --username $MONGO_INITDB_ROOT_USERNAME \
  --password $MONGO_INITDB_ROOT_PASSWORD \
  --authenticationDatabase admin --nsInclude='test.*' --archive=/mongo_backup/test_backup.dump
